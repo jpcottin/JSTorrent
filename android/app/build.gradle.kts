cd899682565016291cba3921d0521703f2598983
Kyle Graehl <kgraehl@gmail.com>
2026-02-03 15:24:52 +0100

    Bump Android version to 1.0.9

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.jstorrent.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 8
-        versionName = "1.0.8"
+        versionCode = 9
+        versionName = "1.0.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
