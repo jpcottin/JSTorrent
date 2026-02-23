62967e0cdedc6d863c496236e12317fbaad2d7d7
Kyle Graehl <kgraehl@gmail.com>
2026-02-23 21:32:37 +0100

    fix deprecated edge-to-edge APIs for Play Store compliance
    Remove android:statusBarColor and android:navigationBarColor from
    themes.xml (deprecated for SDK 35). Add enableEdgeToEdge() to
    PairingApprovalActivity.

--- android/app/src/main/java/com/jstorrent/app/PairingApprovalActivity.kt
+++ android/app/src/main/java/com/jstorrent/app/PairingApprovalActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
@@ -25,6 +26,7 @@ class PairingApprovalActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
 
         val token = intent.getStringExtra(EXTRA_TOKEN) ?: run {
             Log.e(TAG, "Missing token")
