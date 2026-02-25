091bd483c4a6227caf2e66dfb9368a4ce6f8a125
Kyle Graehl <kgraehl@gmail.com>
2026-02-25 14:19:27 +0100

    Release Android v1.0.22

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.jstorrent.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 21
-        versionName = "1.0.21"
+        versionCode = 22
+        versionName = "1.0.22"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
